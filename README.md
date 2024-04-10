9a435a5704ccdc6146129cad2efeb24883140067
Eric Petterson <ericpl.ff@gmail.com>
2024-04-10 19:28:36 -0300

    Adicionado o desafio

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Desafio_Sistema_Bancario_DIO
+# Sistema de Saque e Depósito Bancário
+Este é um programa Python que simula um sistema simples de saque, depósito e extração de dinheiro.
+
+## Funcionalidades
+O programa oferece as seguintes funcionalidades:
+
+### Deposito
+O usuário pode depositar valores positivos.
+Todos os depósitos são armazenados para visualização no extrato.
+### Saque
+O usuário pode realizar saques, desde que respeite as seguintes regras:
+No máximo 3 saques de R$ 500,00 por dia.
+Se não houver saldo suficiente, uma mensagem informando será mostrada.
+Todos os saques são armazenados para visualização no extrato.
+### Extrato
+Mostra todos os movimentos, incluindo depósitos e saques.
+No final, mostra o saldo atual.
+### Uso
+Ao iniciar o programa, você será apresentado a um menu onde poderá selecionar as operações disponíveis:
+
+1. Depositar: Você pode depositar um valor positivo.
+2. Sacar: Você pode sacar um valor, respeitando as regras de limite e saldo disponível.
+3. Extrato: Visualize todos os movimentos e o saldo atual.
+4. Sair: Encerra o programa.
+### Execução
+Para executar o programa, basta rodar o código em Python. Certifique-se de ter um ambiente Python configurado corretamente.
+
+```bash
+python desafio.py
+
+```
---
+++ desafio.py
@@ -0,0 +1,58 @@
+# Regras do desafio
+#Sistema simples de saque, deposito e extração de dinheiro
+# Deposito 
+    # só valores positivos
+    # todos devem ser armazenados para vizualizar no extrato
+    
+# Saque
+    # No maximo 3 saques de 500 por dia
+    # Se não houver saldo, mostrar uma mensagem informando
+    # todos devem ser armazenados para vizualizar no extrato
+    
+# Extrato
+    # deve mostrar todos os movimentos
+    # e no final mostrar o saldo atual
+    
+menu = "\nSelecione uma das opções: \n 1 - Depositar \n 2 - Sacar \n 3 - Extrato \n 4 - Sair \n"
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+
+while True:
+    opcao = input(menu)
+    
+    if opcao == "1":
+        valor = float(input("Quanto deseja depositar? "))
+        if valor > 0:
+            saldo += valor
+            extrato += f"Deposito: R$ {valor:.2f}\n"
+        else:
+            print("O valor precisa ser positivo!")
+    elif opcao == "2":
+        valor = float(input("Quanto deseja sacar? "))
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+        if excedeu_saldo:
+            print("Saldo insuficiente!")
+        elif excedeu_limite:
+            print(f"Limite de saldo de R$ {limite:.2f} excedido!")
+        elif excedeu_saques:
+            print("Limite de saques diarios excedido!")
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+        else:
+            print("O valor precisa ser positivo!")
+    elif opcao == "3":
+        print(f"\n{extrato} \nSaldo: R$ {saldo:.2f}")
+    elif opcao == "4":
+        break
+    else:
+        print("Operação inválida, tente novamente!")
+        
